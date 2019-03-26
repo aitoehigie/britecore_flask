275f29251c1c35775e7020d32f1d33d68b2c179c
aitoehigie <aitoehigie@gmail.com>
2019-03-26 03:24:58 +0100

    Refactor the tests

--- britecore/app/__init__.py
+++ britecore/app/__init__.py
@@ -1,6 +1,6 @@
 
 from flask import Flask
-from config import config
+from .config import config
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
---
+++ britecore/app/config.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = False
+    SECRET_KEY = os.urandom(64)
+    WTF_CSRF_ENABLED = True
+    CSRF_ENABLED = True
+    CSRF_SESSION_KEY = "secret"
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config = {"Development": DevelopmentConfig, "Production": ProductionConfig}
